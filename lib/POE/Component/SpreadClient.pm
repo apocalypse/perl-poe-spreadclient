d0ea32d6cf36a1d839ec76e9de443b1dc9ba84c8
Apocalypse <APOCAL@cpan.org>
2014-11-10 12:21:06 -0800

    fix the spread version for RT#66904

--- lib/POE/Component/SpreadClient.pm
+++ lib/POE/Component/SpreadClient.pm
@@ -9,7 +9,12 @@ use POE::Session;
 use POE::Wheel::ReadWrite;
 use POE::Driver::SpreadClient;
 use POE::Filter::SpreadClient;
-use Spread 3.17.4.4 qw( :MESS :ERROR );
+
+# Thanks to RT#66904 for this craziness!
+{
+	no warnings;
+	use Spread 3.017 qw( :MESS :ERROR );
+}
 
 # Generate our states!
 use parent 'POE::Session::AttributeBased';
